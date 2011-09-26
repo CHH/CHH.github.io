8984b94cc9a36beb0618a4a34127affb746ed9e1
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2011-09-26 22:31:08 +0200

    added comment with twitter link to each article

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 
 $(function() {
     $('#greeting').html(welcomeMessage());
+
+    $(document).delegate('.comment-on-twitter', 'click', function() {
+        var url = encodeURI(
+            document.location.protocol + '//' + document.location.host + 
+            $(this).data('url')
+        );
+        var follow = encodeURI('@yuri41');
+
+        var intent = "https://twitter.com/intent/tweet?url=" + url + '&text=' + follow;
+
+        var w = window.open(
+            intent, 'Kommentiere auf Twitter', 'width=550,height=260,scrollbars=no'
+        );
+        
+        w.focus();
+    });
 });
 
 (function() {
