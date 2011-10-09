14c24429b967c2de19a9fc066e31d8b97f39738f
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2011-10-09 14:33:05 +0200

    Added static CSS, some style updates to blog posts

--- chef/cookbooks/jekyll_server/recipes/default.rb
+++ chef/cookbooks/jekyll_server/recipes/default.rb
@@ -2,9 +2,33 @@
 include_recipe "apt"
 include_recipe "build-essential"
 include_recipe "git"
+include_recipe "nodejs"
 
 package "ruby" do
     action :upgrade
+end
+
+git "/tmp/npmjs" do
+    repository "git://github.com/isaacs/npm.git"
+    action :sync
+    revision "v1.0.94"
+    enable_submodules true
+end
+
+git "/tmp/npmjs/node_modules/lru-cache" do
+    repository "git://github.com/isaacs/node-lru-cache.git"
+    action :sync
+end
+
+execute "install_npm" do
+    command "node cli.js install -g npm"
+    user "root"
+    cwd "/tmp/npmjs"
+end
+
+execute "install_lessjs" do
+    command "npm install -g less"
+    user "root"
 end
 
 execute "install_jekyll" do
